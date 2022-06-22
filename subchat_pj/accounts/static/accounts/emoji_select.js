4eee595035f7157d2b0c2599c2399a5bbec08d19
Mingzhi <alswl_1110@naver.com>
2022-06-22 19:23:51 +0900

    스타일시트 수정!!!

--- subchat_pj/accounts/static/accounts/emoji_select.js
+++ subchat_pj/accounts/static/accounts/emoji_select.js
@@ -1,10 +1,11 @@
 function emoji_change(e){
 // 이모지 클릭시, 내 이모지 span 내용 변경
     document.getElementById('selected_emoji').innerText = e.innerText;
-    document.getElementById('selected_emoji2').innerText = document.getElementById('emoji').value;
+    window.console.log(e);
+//    document.getElementById('selected_emoji2').innerText = document.getElementById('emoji').value;
 };
 
 function emoji_save(){
-    let new_emoji = document.getElementById('selected_emoji').innerText;
+    let new_emoji = document.getElementById('selected_emoji2').innerText;
     window.location.href = '/accounts/emoji_modify/'+new_emoji;
 };
